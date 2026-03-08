2de0cae3447f27e28c8c8d481546a34130e8d36c
devam <devammondal06@gmail.com>
2026-03-07 22:31:31 -0500

    confusion with consts

--- main.cpp
+++ main.cpp
@@ -3,5 +3,8 @@
 
 int main() {
     const int x = 42;
-    const int* ptr = &x;
-}
+    int y = 42;
+    const int* ptr = &x; // this is a pointer to a const int
+    int* const ptrConst = &y; // this is a const pointer to an int
+    const int* const constPtrConst = &x; //this is a const pointer to a const int
+}   
