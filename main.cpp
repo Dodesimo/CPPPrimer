b4807b6ee581fcb60ab9cc411677f8b9e97e3a0f
devam <devammondal06@gmail.com>
2026-03-10 00:08:51 -0400

    class template

--- main.cpp
+++ main.cpp
@@ -2,10 +2,21 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    std::string s {"abcdefghijklmnopqrstuvwxyz"};
-    for (std::size_t i {}; i < s.size(); ++i) {
-        s[i] = std::toupper(s[i]);
+template <typename T>
+class Test {
+public:
+    T f1;
+    T f2;
+    Test() = default;
+    Test (T temp1, T temp2): f1 {temp1}, f2 {temp2} {}
+    T addition() {
+        return f1 + f2;
     }
-    std::cout << s;
+};
+
+int main() {
+    Test<int> t {2, 3};
+    Test<std::string> t2 {"baba", "test"};
+    std::cout << t2.addition();
+    return 0;
 }
